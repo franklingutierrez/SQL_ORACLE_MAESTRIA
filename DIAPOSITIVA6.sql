fe680754faa5caeeead3890d1a1ec46bc2000c86
Franklin Gutierrez <fgutierrezarizaca@gmail.com>
2021-06-30 22:33:12 -0500

    Diapositiva 6-2

--- DIAPOSITIVA6.sql
+++ DIAPOSITIVA6.sql
@@ -16,3 +16,69 @@ BEGIN
 END;
 /
 */
+
+
+/*=======================================
+ \^o^/_______ EJERCICIO 2 ________\^o^/
+=======================================*/
+/*
+SET SERVEROUTPUT ON
+
+DECLARE
+
+ p_empnro EMPLOYEES.EMPLOYEE_ID%TYPE;
+
+ p_salary EMPLOYEES.SALARY%TYPE;
+
+ p_bonus EMPLOYEES.SALARY%TYPE;
+
+BEGIN
+
+ p_empnro := 178;
+
+ SELECT SALARY INTO p_salary
+
+ FROM EMPLOYEES
+
+ WHERE EMPLOYEES.EMPLOYEE_ID = p_empnro;
+
+   
+
+ BEGIN
+
+  CASE
+
+  WHEN p_salary<5000 THEN
+
+   p_bonus:= (10*p_salary)/100;
+
+  WHEN p_salary>=5000 AND p_salary<=10000 THEN
+
+   p_bonus:= (15*p_salary)/100;
+
+  WHEN p_salary>10000 THEN
+
+   p_bonus:= (20*p_salary)/100;
+
+  WHEN p_salary IS NULL THEN
+
+   p_bonus:= 0;
+
+  END CASE;
+
+  DBMS_OUTPUT.PUT_LINE('Número de Empleado: '|| p_empnro ||' SUELDO: '|| p_salary||' Bonificación Resultante: '||(p_bonus));
+
+ END;
+
+END;
+*/
+
+SET SERVEROUTPUT ON
+DECLARE
+  p_empnro INTEGER := 100;
+  p_salary EMPLOYEES.SALARY%type;
+  p_bono NUMERIC(8)
+BEGIN
+END;
+/
+  
